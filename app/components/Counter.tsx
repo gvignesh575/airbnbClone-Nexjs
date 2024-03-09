6738fd266e66c12527673e56c4f45fb662ad0c8b
GVignesh-SD <gvignesh380980@gmail.com>
2024-03-09 15:43:24 +0530

    implemented creating homes functionality and added the maps for the address

--- app/components/Counter.tsx
+++ app/components/Counter.tsx
@@ -1,15 +1,40 @@
+"use client";
 import { Button } from "@/components/ui/button";
-import { Minus } from "lucide-react";
+import { Minus, Plus } from "lucide-react";
+import { useState } from "react";
 
-export function Counter() {
+export function Counter({ name }: { name: string }) {
+  const [amount, setAmount] = useState(0);
+
+  function increase() {
+    setAmount(amount + 1);
+  }
+
+  function decrease() {
+    if (amount > 0) {
+      setAmount(amount - 1);
+    }
+  }
+
   return (
-    <div className="flex items-center gap-x-4">
-      <Button variant={"outline"} size={"icon"} type="button">
+    <div className="flex items-center gap-x-4 ">
+      <input type="hidden" name={name} value={amount} />
+      <Button
+        variant={"outline"}
+        size={"icon"}
+        type="button"
+        onClick={decrease}
+      >
         <Minus className="h-4 w-4 text-primary" />
       </Button>
-      <p>0</p>
-      <Button variant={"outline"} size={"icon"} type="button">
-        <Minus className="h-4 w-4 text-primary" />
+      <p className="font-medium text-lg">{amount}</p>
+      <Button
+        variant={"outline"}
+        size={"icon"}
+        type="button"
+        onClick={increase}
+      >
+        <Plus className="h-4 w-4 text-primary" />
       </Button>
     </div>
   );
--- app/create/[id]/description/page.tsx
+++ app/create/[id]/description/page.tsx
@@ -1,10 +1,18 @@
+import { CreateDescription } from "@/app/actions";
 import { Counter } from "@/app/components/Counter";
+import { CreationBottomBar } from "@/app/components/CreationBottomBar";
 import { Card, CardHeader } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 
-export default function DescriptionPage() {
+export default function DescriptionPage({
+  params,
+}: {
+  params: {
+    id: string;
+  };
+}) {
   return (
     <>
       <div className="w-3/5 mx-auto">
@@ -12,7 +20,8 @@ export default function DescriptionPage() {
           Please describe your home as good as you can!
         </h2>
       </div>
-      <form action="">
+      <form action={CreateDescription}>
+        <input type="hidden" name="homeId" value={params.id} />
         <div className="mx-auto w-3/5 mt-10 flex flex-col gap-y-5 mb-36">
           <div className="flex flex-col gap-y-2">
             <Label>Title</Label>
@@ -49,16 +58,35 @@ export default function DescriptionPage() {
             <CardHeader className="flex flex-col gap-y-5">
               <div className="flex items-center justify-between">
                 <div className="flex flex-col">
-                  <h2 className="underline font-medium">Guest</h2>
+                  <h2 className="underline font-medium">Guests</h2>
                   <p className="text-muted-foreground text-sm">
                     How many guests do you want?
                   </p>
                 </div>
-                <Counter />
+                <Counter name="guest" />
+              </div>
+              <div className="flex items-center justify-between">
+                <div className="flex flex-col">
+                  <h2 className="underline font-medium">Rooms</h2>
+                  <p className="text-muted-foreground text-sm">
+                    How many guests do you want?
+                  </p>
+                </div>
+                <Counter name="room" />
+              </div>
+              <div className="flex items-center justify-between">
+                <div className="flex flex-col">
+                  <h2 className="underline font-medium">Bathrooms</h2>
+                  <p className="text-muted-foreground text-sm">
+                    How many Bathrooms do you have?
+                  </p>
+                </div>
+                <Counter name="bathroom" />
               </div>
             </CardHeader>
           </Card>
         </div>
+        <CreationBottomBar />
       </form>
     </>
   );
