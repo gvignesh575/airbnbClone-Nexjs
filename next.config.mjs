e5bae298698cada2148e1d4a5f93676ff6ff055c
GVignesh-SD <gvignesh380980@gmail.com>
2024-03-10 00:15:45 +0530

    implemented listing the data in the homepage

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,11 @@ const nextConfig = {
         protocol: "https",
         port: "",
       },
+      {
+        hostname: "cvcnrwrxmxifhzdwejub.supabase.co",
+        protocol: "https",
+        port: "",
+      },
     ],
   },
 };
