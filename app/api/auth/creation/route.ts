c103ed3ee8359e5867c3c0fc490bac8261983ca4
GVignesh-SD <gvignesh380980@gmail.com>
2024-03-14 01:46:34 +0530

    changed the url

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -32,5 +32,5 @@ export async function GET() {
     });
   }
 
-  return NextResponse.redirect("http://localhost:3000");
+  return NextResponse.redirect("https://project-airbnb-indol.vercel.app");
 }
